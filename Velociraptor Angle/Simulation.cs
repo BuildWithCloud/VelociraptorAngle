afd2384ad8bc059a534654202db0a781136efd85
BuildWithCloud <itried100bears@outlook.com>
2024-03-29 22:11:50 +0000

    Bunch of debug code

--- Velociraptor Angle/Simulation.cs
+++ Velociraptor Angle/Simulation.cs
@@ -13,8 +13,8 @@ public class Simulation
     public Simulation()
     {
         _fred = new Fred(GetStartingAngle());
-        _injuredVelociraptor = new InjuredVelociraptor(new double[] { 0, 10/Math.Cos(double.DegreesToRadians(30)) });
-        _healthyVelociraptor = new HealthyVelociraptor(new double[] { 10, -10 * Math.Tan(double.DegreesToRadians(30)) });
+        _injuredVelociraptor = new InjuredVelociraptor(new double[] { 0, 10/Math.Cos(0.523599) });
+        _healthyVelociraptor = new HealthyVelociraptor(new double[] { 10, -10 * Math.Tan(0.523599) });
     }
 
     public double Simulate()
@@ -29,14 +29,22 @@ public class Simulation
             _injuredVelociraptor.CalculateNewPosition(TimeStep);
             _time += TimeStep;
             Console.WriteLine("h" + Utilities.GetDistanceBetween(_fred, _healthyVelociraptor));
+            //Console.WriteLine("x" + _healthyVelociraptor.Velocity[0] + " y" + _healthyVelociraptor.Velocity[1] + " ");
             // Console.WriteLine("i" + Utilities.GetDistanceBetween(_fred, _injuredVelociraptor));
+            //Console.WriteLine(double.RadiansToDegrees(_healthyVelociraptor.GetAngleTo(_fred)));
+            //Console.WriteLine("x" + _fred.Position[0] + " y" + _fred.Position[1] + " ");
+            if (_time > 10)
+            {
+                Console.WriteLine("Time limit reached");
+                break;
+            }
         }
         return _time;
     }
 
     private double GetStartingAngle()
     {
-        Console.WriteLine("Please enter starting angle (degrees)");
+        Console.WriteLine("Please enter starting angle (radians)");
         string? input = null;
         var angle = 0.0; // degrees
         while (input == null)
@@ -56,7 +64,8 @@ public class Simulation
                 Console.WriteLine("Invalid input. Please enter a number.");
             }
         }
-        return double.DegreesToRadians(angle);
+
+        return angle;
 
     }
 }
