56ec1dcc844ef9d9c6ed4f564f4aa34cbe8ed1e8
BuildWithCloud <itried100bears@outlook.com>
2024-03-30 10:55:58 +0000

    Cleanup and more simulation settings defined in CTOR, allowing smart angle search

--- Velociraptor Angle/Simulation.cs
+++ Velociraptor Angle/Simulation.cs
@@ -1,35 +1,26 @@
 ﻿namespace Velociraptor_Angle;
 
-public class Simulation
+public class Simulation(double startingAngle, double timeStep, double precision)
 {
-    private readonly Fred _fred;
-    private readonly InjuredVelociraptor _injuredVelociraptor;
-    private readonly HealthyVelociraptor _healthyVelociraptor;
+    private readonly Fred _fred = new(startingAngle);
+    private readonly InjuredVelociraptor _injuredVelociraptor = new(new double[] { 0, 10/Math.Cos(0.523599) });
+    private readonly HealthyVelociraptor _healthyVelociraptor = new(new double[] { 10, -10 * Math.Tan(0.523599) });
     private double _time;
-    private const double TimeStep = 0.0001; 
-    private const double Precision = 0.1;
-    
-    
-    public Simulation()
-    {
-        _fred = new Fred(GetStartingAngle());
-        _injuredVelociraptor = new InjuredVelociraptor(new double[] { 0, 10/Math.Cos(0.523599) });
-        _healthyVelociraptor = new HealthyVelociraptor(new double[] { 10, -10 * Math.Tan(0.523599) });
-    }
+
 
     public ReturnFromSim Simulate()
     {
-        while(Utilities.GetDistanceBetween(_fred, _healthyVelociraptor) > Precision && Utilities.GetDistanceBetween(_fred, _injuredVelociraptor) > Precision)
+        while(Utilities.GetDistanceBetween(_fred, _healthyVelociraptor) > precision && Utilities.GetDistanceBetween(_fred, _injuredVelociraptor) > precision)
         {
-            _fred.CalculateNewVelocity(TimeStep, _healthyVelociraptor);
-            _fred.CalculateNewPosition(TimeStep);
-            _healthyVelociraptor.CalculateNewVelocity(TimeStep, _fred);
-            _healthyVelociraptor.CalculateNewPosition(TimeStep);
-            _injuredVelociraptor.CalculateNewVelocity(TimeStep, _fred);
-            _injuredVelociraptor.CalculateNewPosition(TimeStep);
-            _time += TimeStep;
+            _fred.CalculateNewVelocity(timeStep, _healthyVelociraptor);
+            _fred.CalculateNewPosition(timeStep);
+            _healthyVelociraptor.CalculateNewVelocity(timeStep, _fred);
+            _healthyVelociraptor.CalculateNewPosition(timeStep);
+            _injuredVelociraptor.CalculateNewVelocity(timeStep, _fred);
+            _injuredVelociraptor.CalculateNewPosition(timeStep);
+            _time += timeStep;
         }
-        if (Utilities.GetDistanceBetween(_fred, _healthyVelociraptor) < Precision)
+        if (Utilities.GetDistanceBetween(_fred, _healthyVelociraptor) < precision)
         {
             return new ReturnFromSim(_time, WhoWon.Healthy);
         }
@@ -39,29 +30,6 @@ public class Simulation
         }
     }
 
-    private double GetStartingAngle()
-    {
-        Console.WriteLine("Please enter starting angle (radians)");
-        string? input = null;
-        var angle = 0.0; // degrees
-        while (input == null)
-        {
-            try
-            {
-                input = Console.ReadLine();
-                if (input == null)
-                {
-                    throw new Exception();
-                }
-                angle = double.Parse(input);
-                return angle;
-            }
-            catch
-            {
-                Console.WriteLine("Invalid input. Please enter a number.");
-            }
-        }
-        return angle;
-    }
+    
     
 }
