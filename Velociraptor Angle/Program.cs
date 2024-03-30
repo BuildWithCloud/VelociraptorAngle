5e89c12c92458ea1e8cfeeb57e63807af53a6827
BuildWithCloud <itried100bears@outlook.com>
2024-03-30 11:37:15 +0000

    All works now

--- Velociraptor Angle/Program.cs
+++ Velociraptor Angle/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace Velociraptor_Angle;
+﻿using System.Diagnostics.Metrics;
+
+namespace Velociraptor_Angle;
 
 class Program
 {
     static void Main(string[] args)
     {
-        var simulation = new Simulation();
-        Console.WriteLine(simulation.Simulate());
+        double angle = FindOptimalAngle(0, Math.PI/2, 0.0000000000001);
+        Console.WriteLine($"Optimal angle is {double.RadiansToDegrees(angle)}");
+    }
+
+    private static double FindOptimalAngle(double lowerBound, double upperBound, double requiredPrecision)
+    {
+        double precision = 1;
+        double angle = (lowerBound + upperBound) / 2;
+        while(upperBound - lowerBound > requiredPrecision)
+        {
+            angle = (lowerBound + upperBound) / 2;
+            Console.WriteLine($"Simulating angle {double.RadiansToDegrees(angle)}, precision {precision}");
+            var sim = new Simulation(angle, precision);
+            var result = sim.Simulate();
+            precision = result.Margin / 2;
+            if(result.WhoWon == WhoWon.Healthy)
+            {
+                lowerBound = angle;
+                Console.WriteLine($"Healthy Won in {result.Time} by {result.Margin}");
+            }
+            else
+            {
+                upperBound = angle;
+                Console.WriteLine($"Injured Won in {result.Time} by {result.Margin}");
+            }
+
+        }
+        return angle;
     }
 }
--- Velociraptor Angle/Simulation.cs
+++ Velociraptor Angle/Simulation.cs
@@ -1,24 +1,25 @@
 ﻿namespace Velociraptor_Angle;
 
-public class Simulation(double startingAngle, double timeStep, double precision)
+public class Simulation(double startingAngle, double precision)
 {
     private readonly Fred _fred = new(startingAngle);
     private readonly InjuredVelociraptor _injuredVelociraptor = new(new double[] { 0, 10/Math.Cos(0.523599) });
     private readonly HealthyVelociraptor _healthyVelociraptor = new(new double[] { 10, -10 * Math.Tan(0.523599) });
     private double _time;
+    private readonly double _timeStep = precision/31; // This is the worst case closing speed (25 + 6) this would never happen, but is safe
 
 
     public ReturnFromSim Simulate()
     {
         while(Utilities.GetDistanceBetween(_fred, _healthyVelociraptor) > precision && Utilities.GetDistanceBetween(_fred, _injuredVelociraptor) > precision)
         {
-            _fred.CalculateNewVelocity(timeStep, _healthyVelociraptor);
-            _fred.CalculateNewPosition(timeStep);
-            _healthyVelociraptor.CalculateNewVelocity(timeStep, _fred);
-            _healthyVelociraptor.CalculateNewPosition(timeStep);
-            _injuredVelociraptor.CalculateNewVelocity(timeStep, _fred);
-            _injuredVelociraptor.CalculateNewPosition(timeStep);
-            _time += timeStep;
+            _fred.CalculateNewVelocity(_timeStep, _healthyVelociraptor);
+            _fred.CalculateNewPosition(_timeStep);
+            _healthyVelociraptor.CalculateNewVelocity(_timeStep, _fred);
+            _healthyVelociraptor.CalculateNewPosition(_timeStep);
+            _injuredVelociraptor.CalculateNewVelocity(_timeStep, _fred);
+            _injuredVelociraptor.CalculateNewPosition(_timeStep);
+            _time += _timeStep;
         }
         if (Utilities.GetDistanceBetween(_fred, _healthyVelociraptor) < precision)
         {
