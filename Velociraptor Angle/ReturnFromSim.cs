923e7b22763c98942c34993e7136aa24f8d3281a
BuildWithCloud <itried100bears@outlook.com>
2024-03-30 10:51:47 +0000

    New return system from simulation to allow automated searching for optimum angle

---
+++ Velociraptor Angle/ReturnFromSim.cs
@@ -0,0 +1,13 @@
+﻿namespace Velociraptor_Angle;
+
+public struct ReturnFromSim(double time, WhoWon whoWon)
+{
+    public readonly double Time = time;
+    public readonly WhoWon WhoWon = whoWon;
+}
+
+public enum WhoWon
+{
+    Healthy,
+    Injured,
+}
--- Velociraptor Angle/Simulation.cs
+++ Velociraptor Angle/Simulation.cs
@@ -6,7 +6,7 @@ public class Simulation
     private readonly InjuredVelociraptor _injuredVelociraptor;
     private readonly HealthyVelociraptor _healthyVelociraptor;
     private double _time;
-    private const double TimeStep = 0.00001;
+    private const double TimeStep = 0.0001; 
     private const double Precision = 0.1;
     
     
@@ -17,7 +17,7 @@ public class Simulation
         _healthyVelociraptor = new HealthyVelociraptor(new double[] { 10, -10 * Math.Tan(0.523599) });
     }
 
-    public double Simulate()
+    public ReturnFromSim Simulate()
     {
         while(Utilities.GetDistanceBetween(_fred, _healthyVelociraptor) > Precision && Utilities.GetDistanceBetween(_fred, _injuredVelociraptor) > Precision)
         {
@@ -28,18 +28,15 @@ public class Simulation
             _injuredVelociraptor.CalculateNewVelocity(TimeStep, _fred);
             _injuredVelociraptor.CalculateNewPosition(TimeStep);
             _time += TimeStep;
-            //Console.WriteLine("h" + Utilities.GetDistanceBetween(_fred, _healthyVelociraptor));
-            //Console.WriteLine("x" + _healthyVelociraptor.Velocity[0] + " y" + _healthyVelociraptor.Velocity[1] + " ");
-            // Console.WriteLine("i" + Utilities.GetDistanceBetween(_fred, _injuredVelociraptor));
-            Console.WriteLine(double.RadiansToDegrees(_healthyVelociraptor.GetAngleTo(_fred)));
-            //Console.WriteLine("x" + _fred.Position[0] + " y" + _fred.Position[1] + " ");
-            if (_time > 10)
-            {
-                Console.WriteLine("Time limit reached");
-                break;
-            }
         }
-        return _time;
+        if (Utilities.GetDistanceBetween(_fred, _healthyVelociraptor) < Precision)
+        {
+            return new ReturnFromSim(_time, WhoWon.Healthy);
+        }
+        else
+        {
+            return new ReturnFromSim(_time, WhoWon.Injured);
+        }
     }
 
     private double GetStartingAngle()
@@ -64,8 +61,7 @@ public class Simulation
                 Console.WriteLine("Invalid input. Please enter a number.");
             }
         }
-
         return angle;
-
     }
+    
 }
